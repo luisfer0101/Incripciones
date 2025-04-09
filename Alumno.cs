aad825987a3449dcee1b74d7518d30818adb04e6
LuisFer_30 <160041210+luisfer0101@users.noreply.github.com>
2025-04-08 22:31:35 -0600

    CLASES NECESARIAS

---
+++ Alumno.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Incripciones
+{
+    namespace controlDeInscripción
+    {
+        public class Alumno
+        {
+            public string Dpi { get; set; }
+            public string Nombre { get; set; }
+            public string Direccion { get; set; }
+        }
+    }
+
+}
---
+++ Inscripcion.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Incripciones
+{
+    public class Inscripcion
+    {
+        public string DpiAlumno { get; set; }
+        public string CodigoTaller { get; set; }
+        public DateTime FechaInscripcion { get; set; }
+    }
+}
